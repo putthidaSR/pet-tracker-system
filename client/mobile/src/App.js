545fc00cd6eb8a22e4be714299dda6a7b58cc947
putthidaSR <psamrith@uw.edu>
2021-11-15 21:08:33 -0800

    updated library

--- client/mobile/src/App.js
+++ client/mobile/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 /**
  * The first component that the application will render when the app is loaded.
@@ -14,11 +15,14 @@ export default class App extends React.Component {
   render() {
 
     return (
-      <NavigationContainer>
-        <View style={{flex: 1}}>
-          <Text>Main Page</Text>
-        </View>
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <View style={{flex: 1, justifyContent: 'center'}}>
+            <Text>Main Page</Text>
+          </View>
+        </NavigationContainer>
+      </SafeAreaProvider>
+      
     );
   
   }
