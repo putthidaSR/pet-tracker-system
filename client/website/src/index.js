2c9c9dbe804e5eba6fda61144f24b4e4fb078267
putthidaSR <psamrith@uw.edu>
2021-11-15 19:26:32 -0800

    initialize website

---
+++ client/website/src/index.js
@@ -0,0 +1,9 @@
+/**
+ * This is the starting point of the website.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
