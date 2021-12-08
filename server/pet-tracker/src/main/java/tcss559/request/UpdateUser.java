26ab03f5716117dc4c7fccecefa8f16268e81714
putthidaSR <psamrith@uw.edu>
2021-12-07 23:21:02 -0800

    updated dto model classes

--- server/pet-tracker/src/main/java/tcss559/request/UpdateUser.java
+++ server/pet-tracker/src/main/java/tcss559/request/UpdateUser.java
@@ -2,9 +2,52 @@ package tcss559.request;
 
 public class UpdateUser {
 
+	private String loginName;
+	private String loginPassword;
+	private String badgeNumber;
+	private String email;
+	private String phoneNumber;
 	private String address;
 	
-	private String contact;
+	public String getLoginName() {
+		return loginName;
+	}
+
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+
+	public String getLoginPassword() {
+		return loginPassword;
+	}
+
+	public void setLoginPassword(String loginPassword) {
+		this.loginPassword = loginPassword;
+	}
+
+	public String getBadgeNumber() {
+		return badgeNumber;
+	}
+
+	public void setBadgeNumber(String badgeNumber) {
+		this.badgeNumber = badgeNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 
 	public String getAddress() {
 		return address;
@@ -13,15 +56,4 @@ public class UpdateUser {
 	public void setAddress(String address) {
 		this.address = address;
 	}
-
-	public String getContact() {
-		return contact;
-	}
-
-	public void setContact(String contact) {
-		this.contact = contact;
-	}
-	
-	
-
 }
