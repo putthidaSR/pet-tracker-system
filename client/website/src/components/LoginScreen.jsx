2245b5f589e91bfa47b240e340cd1e1f6100a997
putthidaSR <psamrith@uw.edu>
2021-12-09 01:48:34 -0800

    done with login website

--- client/website/src/components/LoginScreen.jsx
+++ client/website/src/components/LoginScreen.jsx
@@ -1,52 +1,109 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { Component } from "react";
+import { Alert } from 'antd';
+import axios from "axios";
 
+import appLogo from '../assets/app-logo.png';
+
+/**
+ * This component represents login screen when user launches the website for the first time
+ * or for user who is not authenticated yet.
+ * 
+ * Date: 12/9/2021
+ * Author: Putthida Samrith
+ */
 export default class Login extends Component {
 
     constructor(props) {
         super(props);
 
         this.state = {
-            username: ''
+            username: '',
+            password: '',
+            isError: false,
+            isLoading: false // flag to track if the page is loading
         };
     }
 
-    handleLogin = (e) => {
+    /**
+     * Handle the action when user presses Login.
+     */
+    handleLogin = async(e) => {
 
         e.preventDefault();
-        console.log('You clicked submit.');
 
-        /* Redirect the sign in page to home page*/
-        this.props.history.push('/home');
+        // Check if username or password is empty
+        if (this.state.username === '' || this.state.password === '') {
+            this.setState({isError: true});
+            return;
+        }
 
-        // fetch('http://localhost:8080/login', {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify(this.state.username, this.state.password)
-        //     })
-        //     .then(data => data.json())
+        // Call the backend API to login to the website with the provided username and password
+        await axios({
+            method: 'POST',
+            url: 'http://localhost:8080/PawTracker/app/login',
+            data: {
+                loginName: this.state.username,
+                loginPassword: this.state.password
+            },
+            headers: {'user_role': `Admin`},
+            mode: 'no-cors'
+        })
+        .then((response) => {
+
+            console.log('successfully login');
+
+            // Redirect the sign in page to home page if login successful
+            this.props.history.push('/home');
+
+        }, (error) => {
+            if (error.response) {
+                // client received an error response (5xx, 4xx)
+                console.log(error.response);
+                } else if (error.request) {
+                // client never received a response, or request never left
+                console.log(error.request);
+                } else {
+                console.error(error.message);
+            }
+
+            alert('Fail to loigin. Please try again!');
+
+        });
     }
 
     render() {
         return (
             <div className="auth-inner">
 
+                {/** Display app logo */}
+                <div class="center-image">
+                    <img src={appLogo} />
+                </div>
+                <br />
                 <form onSubmit={this.handleLogin}>
-                    <h3>Welcome...</h3>
+               
+                    <h3>Welcome Back, Admin!</h3>
 
+                    {/** Display username field */}
                     <div className="form-group">
-                        <label>Email address</label>
-                        <input type="email" className="form-control" placeholder="Enter email" />
+                        <label>Username</label> <label style={{color: 'red'}}>*</label>
+                        <input type="text" className="form-control" placeholder="Enter username" 
+                          value={this.state.username} 
+                          onChange={(event) => {this.setState({username: event.target.value});}} />
                     </div>
-
                     <br />
+
+                    {/** Display password field */}
                     <div className="form-group">
-                        <label>Password</label>
-                        <input type="password" className="form-control" placeholder="Enter password" />
+                        <label>Password</label> <label style={{color: 'red'}}>*</label>
+                        <input type="password" className="form-control" placeholder="Enter password" 
+                        value={this.state.password} 
+                        onChange={(event) => {this.setState({password: event.target.value});}} />
                     </div>
-
                     <br />
+
+                    {/** Display checkbox field */}
                     <div className="form-group">
                         <div className="custom-control custom-checkbox">
                             <input type="checkbox" className="custom-control-input" id="customCheck1" />
@@ -55,12 +112,12 @@ export default class Login extends Component {
                     </div>
                     <br />
 
-                    <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                    {/** Handle submit button click */}
+                    <button type="submit" className="btn btn-primary btn-block">Login as Admin</button>
                     <br />
 
-                    <p className="forgot-password text-right">
-                        <a href="/sign-up">Create a new account</a>
-                    </p>
+                    {/** Display error if username or password field is empty */}
+                    {this.state.isError && <Alert message="Please enter username and password." type="error" showIcon />}
 
                 </form>
             </div>
